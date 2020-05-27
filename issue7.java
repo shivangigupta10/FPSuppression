4e022cc526a22c545207efd0f2488ee01a84f2c2
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-05-27 13:12:18 +0530

    Update issue7.java

--- issue7.java
+++ issue7.java
@@ -2,6 +2,7 @@
 //
 //
 .executeQuery(something)
+ .executeQuery(something)
  System.out.println("Generation password reset link for " + username);
  System.out.println("Created password reset link: " + new PasswordResetLink().createPasswordReset(username, key));
 
