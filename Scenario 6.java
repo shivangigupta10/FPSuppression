1ac140444a81a6372e087fad900cbe5b889f6031
shivangigupta10 <66000600+shivangigupta10@users.noreply.github.com>
2020-05-29 11:50:22 +0530

    Add files via upload

---
+++ Scenario 6.java
@@ -0,0 +1,5 @@
+st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
+st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
+st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
+st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
+st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
